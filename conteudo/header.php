e3569dec71063aadb39ddd3f0de36a79f77bb96b
bl4nc <40602200+bl4nc@users.noreply.github.com>
2020-06-09 18:42:38 -0300

    Efeitos de zoom e hover

--- conteudo/header.php
+++ conteudo/header.php
@@ -1,25 +1,41 @@
     <div class="flex">
         <div class="container">
-            <!-- Image and text -->
-            <nav class=" navbar navbar-expand-lg" id="navbarText">
-                <div class="row">
-                    <div class="col-sm">
-                        <a class="navbar-brand" href="#">
-                            <img src="midia/logo-covid-feira.png">
+
+            <nav class="navbar navbar-expand- " id="navbarText">
+                <ul class="navbar-nav mr-auto">
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <div class="zoom">
+                                <img src="midia/logo-covid-feira.png" id="logo">
+                            </div>
+
+                        </a>
+                    </li>
+
+                </ul>
+
+                <div class="col-">
+                    <div id="mkcovid">
+                        <a class="navbar-text h5" href="#">
+                            <span class="navbar-text makeLogo" id="opc">
+                                make
+                                <span class="covid19t" id="opc">/COVID19 </span>
+                            </span>
+
                         </a>
                     </div>
-                    <div class="col-sm">
-                        <span class="form-inline makeLogo">make</span>
-                    </div>
-
                 </div>
-
-
-
-
-
-
             </nav>
+
+
+
+
+
+
+
+
+
 
         </div>
     </div>
