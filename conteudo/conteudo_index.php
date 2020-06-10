4ff165da40a664744b3ceb6ff9b807f59e14161a
João Neto <joaoneto.20fsa@gmail.com>
2020-06-10 15:32:57 -0300

    Parte dos cards contruída

--- conteudo/conteudo_index.php
+++ conteudo/conteudo_index.php
@@ -1,3 +1,63 @@
 <main class="container">
-    
+    <section class="secao_cards mt-3">
+        <!-- <div class="row">
+            <div class="col-sm-4">
+                <div class="card text-white bg-vermelho mb-3" style="max-width: 20rem;">
+                    <div class="card-body">
+                        <h5>QUANTIDADE DE INFECTADOS</h5>
+                        <p class="pt-3">
+                           1.025 
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-4">
+                <div class="card text-white bg-verde mb-3" style="max-width: 20rem;">
+                    <div class="card-body">
+                        <h5>QUANTIDADE DE CURADOS</h5>
+                        <p class="pt-3">
+                            233
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-4">
+                <div class="card text-white bg-amarelo mb-3" style="max-width: 20rem;">
+                    <div class="card-body">
+                        <h5>QUANTIDADE DE GASTOS</h5>
+                        <p class="pt-3">
+                            1.2mi
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div> -->
+        <h4>Dados do <strong>Coronavírus</strong></h4>
+        <p>Data do boletim: 10/06/2020</p>
+
+        <div class="card-deck">
+            <div class="card bg-vermelho">
+                <div class="card-body">
+                <h5 class="card-title">QUANTIDADE DE INFECTADOS</h5>
+                <p class="card-text">1.025</p>
+                </div>
+            </div>
+
+            <div class="card bg-verde">
+                <div class="card-body">
+                <h5 class="card-title">QUANTIDADE DE CURADOS</h5>
+                <p class="card-text">233</p>
+                </div>
+            </div>
+
+            <div class="card bg-amarelo">
+                <div class="card-body">
+                <h5 class="card-title">QUANTIDADE DE GASTOS</h5>
+                <p class="card-text">1.2mi</p>
+                </div>
+            </div>
+        </div>
+    </section> 
 </main>
