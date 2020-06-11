e086995cfd7427aef5cbe43e0376414d949a52e2
João Neto <joaoneto.20fsa@gmail.com>
2020-06-10 23:04:46 -0300

    Ajustes no código

--- conteudo/conteudo_index.php
+++ conteudo/conteudo_index.php
@@ -1,39 +1,5 @@
 <main class="container">
     <section class="secao_cards mt-3">
-        <!-- <div class="row">
-            <div class="col-sm-4">
-                <div class="card text-white bg-vermelho mb-3" style="max-width: 20rem;">
-                    <div class="card-body">
-                        <h5>QUANTIDADE DE INFECTADOS</h5>
-                        <p class="pt-3">
-                           1.025 
-                        </p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-sm-4">
-                <div class="card text-white bg-verde mb-3" style="max-width: 20rem;">
-                    <div class="card-body">
-                        <h5>QUANTIDADE DE CURADOS</h5>
-                        <p class="pt-3">
-                            233
-                        </p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-sm-4">
-                <div class="card text-white bg-amarelo mb-3" style="max-width: 20rem;">
-                    <div class="card-body">
-                        <h5>QUANTIDADE DE GASTOS</h5>
-                        <p class="pt-3">
-                            1.2mi
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div> -->
         <h4>Dados do <strong>Coronavírus</strong></h4>
         <p>Data do boletim: 10/06/2020</p>
 
@@ -220,6 +186,5 @@
                 </tbody>
             </table>
         </div>
-
     </section>
 </main>
