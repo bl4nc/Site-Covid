6306edab118101b7376c76a82b09d75e2ad53811
João Neto <joaoneto.20fsa@gmail.com>
2020-06-10 23:43:00 -0300

    Update header.php

--- conteudo/header.php
+++ conteudo/header.php
@@ -18,35 +18,3 @@
         </div>
     </div>
 </header>
-<!---    
-        <div class="container">
-
-            <nav class="navbar navbar-expand- " id="navbarText">
-                <ul class="navbar-nav mr-auto">
-
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                            <div class="zoom">
-                                <img src="midia/logo-covid-feira.png" id="logo">
-                            </div>
-
-                        </a>
-                    </li>
-
-                </ul>
-
-                <div class="col-">
-                    <div id="mkcovid">
-                        <a class="navbar-text h5" href="#">
-                            <span class="navbar-text makeLogo" id="opc">
-                                make
-                                <span class="covid19t" id="opc">/COVID19 </span>
-                            </span>
-
-                        </a>
-                    </div>
-                </div>
-            </nav>
-            
-        </div> 
-</div>-->
