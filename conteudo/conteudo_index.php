159a64eb34f46451eaea7c53caaa7d8388c6face
João Neto <joaoneto.20fsa@gmail.com>
2020-06-12 13:11:59 -0300

    Mais ajustes no site

--- conteudo/conteudo_index.php
+++ conteudo/conteudo_index.php
@@ -1,8 +1,21 @@
 <main class="container">
     <section class="secao_cards mt-3">
-        <h4>Dados do <strong>Coronavírus</strong></h4>
+        <!-- <h4>Dados do <strong>Coronavírus</strong></h4>
         <p>Dados até: <b><?php echo date('d/m/yy', strtotime(dtBoletim())); ?></b><br>
             <span style="color: #007c34;font-size: 16px">Dados retirados de: https://www.combateaocoronavirus.feira.br/</span>
+        </p> -->
+
+        <h4 class="mb-2">Dados do Coronavírus até:
+            <strong>
+                 <?php echo date('d/m/yy', strtotime(dtBoletim())); ?>
+            </strong>
+        </h4>
+        <p>
+            <strong style="font-size: 16px">Dados retirados de 
+                <a rel="noopener noreferrer" target="_blank" href="https://www.combateaocoronavirus.feira.br">
+                    Combate ao Corona Vírus 
+                </a>
+            </strong>
         </p>
         
 
@@ -111,11 +124,16 @@
         </div>
     </section>
 
-
-
     <section class="secao_gastos mt-5"><!-- SEÇÃO COM OS GASTOS -->
-        <h4>Valores Investidos no Combate a COVID-19 FSA</h4>
-        <p style="color: #007c34;font-size: 16px">Dados retirados de: http://www.transparencia.feiradesantana.ba.gov.br/index.php?view=despesascovid</p>
+            <h4>Valores Investidos no Combate a COVID-19 FSA</h4>
+            <p>
+                <strong> 
+                    Dados retirados do 
+                    <a rel="noopener noreferrer" target="_blank" href="http://www.transparencia.feiradesantana.ba.gov.br/index.php?view=despesascovid">
+                        Portal de Transparência
+                    </a>
+                </strong>
+            </p>
 
         <div class="table-responsive">
             <table class="table table-borderless"  id="example">
@@ -174,14 +192,14 @@
         <h4>Últimas informações</h4>
         <div class="container">
             <div class="row">
-                <div class="col-sm-4 mt-4" style="height: 400px;overflow-x: hidden;overflow-y: auto">
+                <div class="col-sm-4 mt-4 mb-4" style="height: 400px;overflow-x: hidden;overflow-y: auto">
                     <a class="twitter-timeline" href="https://twitter.com/PrefeituraFeira?ref_src=twsrc%5Etfw">&nbsp;</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
                 </div>
-                <div class="col-sm-4 mt-4"  style="height: 400px;overflow-x: hidden;overflow-y: auto">
+                <div class="col-sm-4 mt-4 mb-4"  style="height: 400px;overflow-x: hidden;overflow-y: auto">
                     <a class="twitter-timeline" href="https://twitter.com/colbertprefeito?ref_src=twsrc%5Etfw">&nbsp;</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 
                 </div>
-                <div class="col-sm-4 mt-4"  style="height: 400px;overflow-x: hidden;overflow-y: auto">
+                <div class="col-sm-4 mt-4 mb-4"  style="height: 400px;overflow-x: hidden;overflow-y: auto">
                     <a class="twitter-timeline" href="https://twitter.com/Acordacidade?ref_src=twsrc%5Etfw">&nbsp;</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
                 </div>
             </div>
