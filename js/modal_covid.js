3bcdab8afb5feb8736b2addf007b217d5dd7ae69
João Neto <joaoneto.20fsa@gmail.com>
2020-07-12 23:30:15 -0300

    Update modal_covid.js

--- js/modal_covid.js
+++ js/modal_covid.js
@@ -1,13 +1,19 @@
 (function modal_covid(){
     const clickable = document.querySelectorAll(".clickable");
-    // abrindo modal
+    const overlay = document.querySelector(".overlay");
+    const modal_close = document.querySelector(".modal_close");
+    const closeItems = [ overlay, modal_close];
+    // abrindo e fechando modal
     clickable.forEach(card => {
         card.addEventListener("click", () => {
-            const overlay = document.querySelector(".overlay");
             overlay.classList.toggle("modal_covid_open");
-            console.log(overlay);
         })
     })
-    // fechando modal
-    
+
+    closeItems.forEach(item => {
+        item.addEventListener("click", () => {
+            item.classList.toggle("modal_covid_open");
+        })
+    });
+
 })();
